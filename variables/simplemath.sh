2d4d413df094888ba3559f87dc6e07234574acd4
Gokul <inboxtogokul@gmail.com>
2024-02-22 17:39:24 +0530

    Update simplemath.sh

--- variables/simplemath.sh
+++ variables/simplemath.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+## Simple Math: Write a script that calculates the sum of two numbers and stores the result in a variable. Then, print the result.
+
 num1=5
 num2=5
 sum=$(( num1 + num2 ))
